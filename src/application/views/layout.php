b0cde16523f497418cbebefd88c4dd9e9fc0188c
金子純樹 <kanekojunki@kanekojunjunoMacBook-Pro.local>
2020-04-10 21:47:16 +0900

    ユーザー登録機能まで

---
+++ src/application/views/layout.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mini Blog</title>
+</head>
+
+<body>
+    <div id="header">
+        <h1><a href="<?= $base_url; ?>">Mini Blog</a></h1>
+    </div>
+
+    <div id="main">
+        <?= $_content; ?>
+    </div>
+</body>
+
+</html>
